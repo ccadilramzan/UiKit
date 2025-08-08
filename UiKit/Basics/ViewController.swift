0e4ff86aabb30c4c7078c881f8cf83623d49ad47
ccadilramzan <adilramzan@carecloud.com>
2025-08-08 16:21:03 +0500

    changes in ui

--- UiKit/Basics/ViewController.swift
+++ UiKit/Basics/ViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var greetingLabel: UILabel!
+    
+    @IBOutlet weak var greetingBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //view.backgroundColor = .purple
+        //greetingLabel.text = "Hello, World!"
+        //greet("Adil")
+        
     }
+    
+    func greet(_ name: String) {
+        greetingLabel.text = "Hello, \(name)!"
+    }
+    
+    var isLightMode = true
+    @IBAction func greetingBtnTap(_ sender: Any) {
+        print("Button Tapped")
+        greet("Adil")
+        
+        if isLightMode {
+            view.backgroundColor = .white
+        } else {
+            view.backgroundColor = .purple
+        }
 
-
+        // flip the flag so next tap changes it
+        isLightMode.toggle()
+    }
+    
+    
 }
 
+//#Preview {
+//    ViewController()
+//}
